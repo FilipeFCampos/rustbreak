16af121a785901b956e075aa8968b41a28f51950
leirdan <uouleirdan@gmail.com>
2025-12-01 22:04:41 -0300

    fix(game_flow): max players per session fixed

--- src/game/game_session.rs
+++ src/game/game_session.rs
@@ -7,7 +7,7 @@ use std::io::BufReader;
 use std::path::PathBuf;
 use uuid::Uuid;
 
-pub const MAX_PLAYERS_PER_SESSION: usize = 1;
+pub const MAX_PLAYERS_PER_SESSION: usize = 3;
 
 pub enum GameEvent {
     PlayerJoined(String),
